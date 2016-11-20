d2cf15b10a9e8fd5234036195c2a90580ef48858
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-11-20 12:48:20 +0100

    Sets state via componentWillReceiveProps

--- client/components/GitHub/UserProfile.js
+++ client/components/GitHub/UserProfile.js
@@ -7,9 +7,10 @@ const UserProfile = React.createClass({
     },
     render() {
         return (
-          <div>
+          <div>            
             {this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
             {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
+            {this.props.bio.bio && <li className="list-group-item">Bio: {this.props.bio.bio}</li>}
             {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
             {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
             {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
